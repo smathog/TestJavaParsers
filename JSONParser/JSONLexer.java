96c9fdcedf4537bd54d37bea0e96c45174bdcb28
Scott Mathog <smathog@gmail.com>
2021-01-01 23:03:06 -0800

    Updated main lexer function logic to fix logic error (could not properly update string iteration index based on returned lexer strings, replaced with while loop and list return to update input string)

--- JSONParser/JSONLexer.java
+++ JSONParser/JSONLexer.java
@@ -2,32 +2,51 @@ package com.Parsers.JSONParser;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
 import java.util.ArrayList;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class JSONLexer {
-    private static HashSet<String> JSONWhitespace;
-    private static HashSet<String> JSONSyntax;
+    private static HashSet<Character> JSONWhitespace;
+    private static HashSet<Character> JSONSyntax;
+    private static HashSet<Character> JSONNumberCharacters;
 
     static {
-        JSONWhitespace = Stream.of(" ", "\t", "\b", "\n", "\r")
+        JSONWhitespace = Stream.of(' ', '\t', '\b', '\n', '\r')
                 .collect(Collectors.toCollection(HashSet::new));
         JSONSyntax = Arrays.stream(JSONCharacter.values())
-                .map(JSONCharacter::getField)
+                .map(C -> C.getField().charAt(0))
                 .collect(Collectors.toCollection(HashSet::new));
+        JSONNumberCharacters = IntStream.range(0, 10)
+                .mapToObj(i -> Character.valueOf((char) ('0' + i)))
+                .collect(Collectors.toCollection(HashSet::new));
+
     }
 
-    public static Optional<String> lexString(String input) {
-        return Optional.empty();
+    public static Optional<List<String>> lexString(String input) {
+        StringBuilder sb = new StringBuilder();
+        if (!JSONCharacter.JSON_QUOTE.getField().equals(input.substring(0, 1)))
+            return Optional.empty();
+        input = input.substring(1);
+        input.chars()
+                .takeWhile(c -> !JSONCharacter.JSON_QUOTE.getField().equals(Character.toString(c)))
+                .forEachOrdered(sb::append);
+        if (sb.length() == input.length())
+            throw new IllegalArgumentException("Failed to reach terminal \" at end of input String");
+        else
+            return Optional.of(List.of(sb.toString(), input.substring(sb.length() + 1)));
     }
 
-    public static Optional<String> lexNumber(String input) {
-        return Optional.empty();
+    public static Optional<List<String>> lexNumber(String input) {
+        StringBuilder sb = new StringBuilder();
+        input.chars()
     }
 
-    public static Optional<String> lexBoolean(String input) {
+    public static Optional<List<String>> lexBoolean(String input) {
         return Optional.empty();
     }
 
@@ -35,38 +54,38 @@ public class JSONLexer {
         return Optional.empty();
     }
 
-    public static ArrayList<String> lex(String input) {
-        ArrayList<String> tokens = new ArrayList<>();
-        for (int i = 0; i < input.length(); ++i) {
+    public static List<String> lex(String input) {
+        List<String> tokens = new ArrayList<>();
+        while (!input.isEmpty()) {
             var jsonString = lexString(input);
             if (jsonString.isPresent()) {
-                tokens.add(jsonString.get());
-                i += jsonString.get().length() - 1;
+                tokens.add(jsonString.get().get(0));
+                input = jsonString.get().get(1);
                 continue;
             }
             var jsonNumber = lexNumber(input);
             if (jsonNumber.isPresent()) {
-                tokens.add(jsonNumber.get());
-                i += jsonNumber.get().length() - 1;
+                tokens.add(jsonNumber.get().get(0));
+                input = jsonNumber.get().get(1);
                 continue;
             }
             var jsonBoolean = lexBoolean(input);
             if (jsonBoolean.isPresent()) {
-                tokens.add(jsonBoolean.get());
-                i += jsonBoolean.get().length() - 1;
+                tokens.add(jsonBoolean.get().get(0));
+                input = jsonBoolean.get().get(1);
                 continue;
             }
             var jsonNull = lexNull(input);
             if (jsonNull.isPresent()) {
                 tokens.add(null);
-                i += jsonNull.get().length() - 1;
+                input = jsonBoolean.get();
                 continue;
             }
-            String leadChar = input.substring(i, i + 1);
+            char leadChar = input.charAt(0);
             if (JSONWhitespace.contains(leadChar))
                 continue;
             else if (JSONSyntax.contains(leadChar)) {
-                tokens.add(leadChar);
+                tokens.add(Character.toString(leadChar));
             } else {
                 throw new IllegalArgumentException("Unexpected character: " + leadChar);
             }
