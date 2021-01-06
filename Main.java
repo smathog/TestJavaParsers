ca3ff1a89ef3149874c10198b0749cdf686bd0ae
Scott Mathog <smathog@gmail.com>
2021-01-06 06:54:20 -0800

    updated test main to a user-input evaluation loop

--- Main.java
+++ Main.java
@@ -1,37 +1,26 @@
 package com.Parsers;
 
-import com.Parsers.JSONComponents.JSONComponent;
 import com.Parsers.JSONParser.JSONLexer;
 import com.Parsers.JSONParser.JSONParser;
 import com.Parsers.XMLParser.XMLParser;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        //XML Test main
-        String s1 = "<element attribute1= \"value1\" attribute2 = \"value2\" />";
-        String s2 = "<element></element>";
-        String s3 = "<element>Content</element>";
-        String s4 = "<element attribute1= \"value1\" attribute2 = \"value2\" ></element>";
-        String s5= "<element><nested attribute1= \"value1\" attribute2 = \"value2\" >Content</nested></element>";
-        System.out.println(XMLParser.parse(s5, 0).getFirst());
-        //JSON Test main
-        /*
-        String s1 = "{\"foo\": [1.0, 2, {\"bar\": [2, 3, 4, \"pie\"]}]}";
-        String s2 = "{\"cake\" : 2.0, \"pie\": 3.0}";
-        String s3 = "{\"foo\": [1.0, 2, {\"bar\": [2, 3, 4, \"pie\"]}], \"next\": \"cake\"}";
-        //Sample from blog post
-        var tokens = JSONLexer.lex(s1);
-        System.out.println("===LEXER OUTPUT===");
-        tokens.stream()
-                .map(JSONComponent::JSONRepresentation)
-                .forEach(System.out::println);
-        System.out.println("===LEXER OUTPUT===");
-        System.out.println("===PARSER OUTPUT===");
-        var output = JSONParser.parse(tokens, 0);
-        System.out.println(output.getFirst().JSONRepresentation());
-        System.out.println("===PARSER OUTPUT===");
-
-         */
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Enter JSON or XML: ");
+            String input = scanner.nextLine();
+            if (input.startsWith("<"))
+                System.out.println(XMLParser.parse(input, 0).getFirst());
+            else if (input.startsWith("{"))
+                System.out.println(JSONParser.parse(JSONLexer.lex(input), 0).getFirst().JSONRepresentation());
+            else if (input.equals("exit"))
+                break;
+            else
+                System.out.println("Error: not XML or JSON");
+        }
     }
 }
